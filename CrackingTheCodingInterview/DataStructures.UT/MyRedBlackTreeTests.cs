60f057b48af9904d89313406f2001e0b78ae9ca8
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-08-11 21:43:38 +0300

    MyRedBlackTreeNode implementation, insert root + Unit Tests

---
+++ CrackingTheCodingInterview/DataStructures.UT/MyRedBlackTreeTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace DataStructures.UT
+{
+    public class MyRedBlackTreeTests
+    {
+        [Fact]
+        public void Should_Create_Default()
+        {
+            //arrange
+
+            //act
+            var result = new MyRedBlackTree<int>();
+
+            //assert
+            result.Count.ShouldBeEquivalentTo(0);
+            result.Root.ShouldBeEquivalentTo(null);
+        }
+
+        [Fact]
+        public void Should_Insert_Root()
+        {
+            //arrange
+            var result = new MyRedBlackTree<int>();
+
+            //act
+            result.Insert(1);
+
+            //assert
+            result.Count.ShouldBeEquivalentTo(1);
+
+            result.Root.Data.ShouldBeEquivalentTo(1);
+            result.Root.Color.ShouldBeEquivalentTo(NodeColor.Black);
+            result.Root.Left.Color.ShouldBeEquivalentTo(NodeColor.Black);
+            result.Root.Right.Color.ShouldBeEquivalentTo(NodeColor.Black);
+            result.Root.Left.IsNull.ShouldBeEquivalentTo(true);
+            result.Root.Right.IsNull.ShouldBeEquivalentTo(true);
+            result.Root.Left.Parent.ShouldBeEquivalentTo(result.Root);
+            result.Root.Right.Parent.ShouldBeEquivalentTo(result.Root);
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/DataStructures/MyRedBlackTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public enum NodeColor
+    {
+        Red,
+        Black
+    }
+    public class MyRedBlackTreeNode<T>
+    {
+        public T Data { get; private set; }
+        public MyRedBlackTreeNode<T> Left { get; set; }
+        public MyRedBlackTreeNode<T> Right { get; set; }
+        public MyRedBlackTreeNode<T> Parent { get; set; }
+
+        public bool IsNull { get; set; }
+        public NodeColor Color { get; set; }
+
+        public MyRedBlackTreeNode(T data)
+        {
+            Data = data;
+            Color = NodeColor.Red;
+            Left = new MyRedBlackTreeNode<T>(true, this);
+            Right = new MyRedBlackTreeNode<T>(true, this);
+        }
+
+        public MyRedBlackTreeNode(bool isNull, MyRedBlackTreeNode<T> parent)
+        {
+            IsNull = isNull;
+            Parent = parent;
+            Color = NodeColor.Black;
+        }
+
+        public MyRedBlackTreeNode(T data, NodeColor color):this(data)
+        {
+            Color = color;
+        }
+
+        public MyRedBlackTreeNode(T data,
+            NodeColor color,
+            MyRedBlackTreeNode<T> left,
+            MyRedBlackTreeNode<T> right) : this(data, color)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public MyRedBlackTreeNode(T data,
+            NodeColor color,
+            MyRedBlackTreeNode<T> left,
+            MyRedBlackTreeNode<T> right,
+            MyRedBlackTreeNode<T> parent) : this(data, color, left, right)
+        {
+            Parent = parent;
+        }
+    }
+
+    public class MyRedBlackTree<T>
+    {
+        public int Count { get; private set; }
+        public MyRedBlackTreeNode<T> Root { get; private set; }
+
+        public MyRedBlackTree()
+        {
+            
+        }
+
+
+        public void Insert(T data)
+        {
+            if(Root == null)
+                Root = new MyRedBlackTreeNode<T>(data, NodeColor.Black);
+
+            Count++;
+        }
+    }
+}

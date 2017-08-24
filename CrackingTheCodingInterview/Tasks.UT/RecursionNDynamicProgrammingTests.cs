dc264dd7790fad24a7cd9a77582b8d13a20f1a55
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-08-24 16:54:37 +0300

    Recursion and Dynamic Programming - Triple Step + Unit Tests

---
+++ CrackingTheCodingInterview/Tasks.UT/RecursionNDynamicProgrammingTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Tasks.UT
+{
+    public class RecursionNDynamicProgrammingTests
+    {
+        public RecursionNDynamicProgrammingTests()
+        {
+            _recursionNDynamicProgramming = new RecursionNDynamicProgramming();
+        }
+        private readonly RecursionNDynamicProgramming _recursionNDynamicProgramming;
+
+        [Fact]
+        public void Triple_Step_Should_Check_First_Three()
+        {
+            //arrange
+
+            //act
+            var first = _recursionNDynamicProgramming.TripleStep(1);
+            var second = _recursionNDynamicProgramming.TripleStep(2);
+            var third = _recursionNDynamicProgramming.TripleStep(3);
+
+            //assert
+            first.ShouldBeEquivalentTo(1);
+            second.ShouldBeEquivalentTo(2);
+            third.ShouldBeEquivalentTo(4);
+        }
+
+        [Fact]
+        public void Triple_Step_Should_Throw_If_Not_In_Range()
+        {
+            //arrange
+
+            //act
+            Action actZero = () => _recursionNDynamicProgramming.TripleStep(0);
+            Action actNegative = () => _recursionNDynamicProgramming.TripleStep(-1);
+
+            //assert
+            actZero.ShouldThrow<ArgumentOutOfRangeException>();
+            actNegative.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void Triple_Step_Should_Check_Example()
+        {
+            //arrange
+
+            //act
+            var first = _recursionNDynamicProgramming.TripleStep(4);
+
+            //assert
+            first.ShouldBeEquivalentTo(7);
+        }
+
+        [Fact]
+        public void Triple_Step_Should_Check_Example_Higher()
+        {
+            //arrange
+
+            //act
+            var first = _recursionNDynamicProgramming.TripleStep(6);
+
+            //assert
+            first.ShouldBeEquivalentTo(24);
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/Tasks/RecursionNDynamicProgramming.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class RecursionNDynamicProgramming
+    {
+        public long TripleStep(int n)
+        {
+            if(n<=0)
+                throw new ArgumentOutOfRangeException();
+
+            long a = 1;
+            long b = 2;
+            long c = 4;
+
+            if (n == 1)
+                return a;
+            if (n == 2)
+                return b;
+
+            for (int i = 3; i < n; i++)
+            {
+                var tempC = c;
+                c = c + b + a;
+                var tempB = b;
+                b = tempC;
+                a = tempB;
+            }
+            return c;
+        }
+    }
+}

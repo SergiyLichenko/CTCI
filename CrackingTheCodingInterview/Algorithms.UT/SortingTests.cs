1d7a4288ef0aa0a7c0dbd67672a415d6df368603
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-09-04 00:20:23 +0300

    Sorting and Searching - Selection Sort + Unit tests

--- CrackingTheCodingInterview/Algorithms.UT/SortingTests.cs
+++ CrackingTheCodingInterview/Algorithms.UT/SortingTests.cs
@@ -10,11 +10,11 @@ namespace Algorithms.UT
 {
     public class SortingTests
     {
-        private readonly Sorting _sorting;
+        private readonly Sorting<int> _sorting;
         private readonly Random _random;
         public SortingTests()
         {
-            _sorting = new Sorting();
+            _sorting = new Sorting<int>();
             _random = new Random();
         }
 
@@ -53,6 +53,46 @@ namespace Algorithms.UT
 
             //Act
             var result = _sorting.BubbleSort(array).ToList();
+
+            //Assert
+            result.Count.ShouldBeEquivalentTo(0);
+        }
+
+        [Fact]
+        public void SelectionSort_Should_Throw_If_Null()
+        {
+            //Arrange
+
+            //Act
+            Action act = () => _sorting.SelectionSort(null).ToArray();
+
+            //Assert
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void SelectionSort_Should_Sort()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 1000).ToArray();
+            array = array.OrderBy(x => _random.Next()).ToArray();
+
+            //Act
+            var result = _sorting.SelectionSort(array).ToList();
+
+            //Assert
+            result.Count.ShouldBeEquivalentTo(array.Length);
+            result.SequenceEqual(array.OrderBy(x => x)).ShouldBeEquivalentTo(true);
+        }
+
+        [Fact]
+        public void SelectionSort_Should_Check_Empty()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 0).ToArray();
+
+            //Act
+            var result = _sorting.SelectionSort(array).ToList();
 
             //Assert
             result.Count.ShouldBeEquivalentTo(0);
--- CrackingTheCodingInterview/Algorithms/Sorting.cs
+++ CrackingTheCodingInterview/Algorithms/Sorting.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Algorithms
 {
-    public class Sorting
+    public class Sorting<T> where T : IComparable
     {
-        public IEnumerable<int> BubbleSort(IEnumerable<int> array)
+        public IEnumerable<T> BubbleSort(IEnumerable<T> array)
         {
             if (array == null)
                 throw new ArgumentNullException();
@@ -17,17 +17,40 @@ namespace Algorithms
             for (int i = 0; i < tempArray.Length; i++)
             {
                 for (int j = i + 1; j < tempArray.Length; j++)
-                {
-                    if (tempArray[j] < tempArray[i])
-                    {
-                        tempArray[i] += tempArray[j];
-                        tempArray[j] = tempArray[i] - tempArray[j];
-                        tempArray[i] -= tempArray[j];
-                    }
-
-                }
+                    if (tempArray[j].CompareTo(tempArray[i]) < 0)
+                        Swap(tempArray, i, j);
                 yield return tempArray[i];
             }
+        }
+
+        public IEnumerable<T> SelectionSort(IEnumerable<T> array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            var tempArray = array.ToArray();
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                int minIndex = i;
+                T minItem = tempArray[i];
+                for (int j = i + 1; j < tempArray.Length; j++)
+                {
+                    if (tempArray[j].CompareTo(minItem) < 0)
+                    {
+                        minItem = tempArray[j];
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                    Swap(tempArray, i, minIndex);
+                yield return tempArray[i];
+            }
+        }
+
+        private static void Swap(T[] tempArray, int i, int j)
+        {
+            var temp = tempArray[i];
+            tempArray[i] = tempArray[j];
+            tempArray[j] = temp;
         }
     }
 }

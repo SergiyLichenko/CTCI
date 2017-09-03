6fcdb3843569b4dd446dc1086c068d3adf8b6f50
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-09-04 00:13:10 +0300

    Sorting and Searching - Bubble sort implementation + Unit Tests

---
+++ CrackingTheCodingInterview/Algorithms.UT/SortingTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.UT
+{
+    public class SortingTests
+    {
+        private readonly Sorting _sorting;
+        private readonly Random _random;
+        public SortingTests()
+        {
+            _sorting = new Sorting();
+            _random = new Random();
+        }
+
+        [Fact]
+        public void BubbleSort_Should_Throw_If_Null()
+        {
+            //Arrange
+
+            //Act
+            Action act = () => _sorting.BubbleSort(null).ToArray();
+
+            //Assert
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void BubbleSort_Should_Sort()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 1000).ToArray();
+            array = array.OrderBy(x => _random.Next()).ToArray();
+
+            //Act
+            var result = _sorting.BubbleSort(array).ToList();
+
+            //Assert
+            result.Count.ShouldBeEquivalentTo(array.Length);
+            result.SequenceEqual(array.OrderBy(x => x)).ShouldBeEquivalentTo(true);
+        }
+
+        [Fact]
+        public void BubbleSort_Should_Check_Empty()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 0).ToArray();
+
+            //Act
+            var result = _sorting.BubbleSort(array).ToList();
+
+            //Assert
+            result.Count.ShouldBeEquivalentTo(0);
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/Algorithms/Sorting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Sorting
+    {
+        public IEnumerable<int> BubbleSort(IEnumerable<int> array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            var tempArray = array.ToArray();
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                for (int j = i + 1; j < tempArray.Length; j++)
+                {
+                    if (tempArray[j] < tempArray[i])
+                    {
+                        tempArray[i] += tempArray[j];
+                        tempArray[j] = tempArray[i] - tempArray[j];
+                        tempArray[i] -= tempArray[j];
+                    }
+
+                }
+                yield return tempArray[i];
+            }
+        }
+    }
+}

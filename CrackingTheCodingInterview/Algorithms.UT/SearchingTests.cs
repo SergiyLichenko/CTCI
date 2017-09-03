a27d302693dbeb0411b5966eeab9e069886a2c53
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-09-04 00:35:05 +0300

    Sorting and Searching - Binary Search iterative and recursive implementations + Unit Tests

---
+++ CrackingTheCodingInterview/Algorithms.UT/SearchingTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.UT
+{
+    public class SearchingTests
+    {
+        private readonly Searching<int> _searching;
+        public SearchingTests()
+        {
+            _searching = new Searching<int>();
+        }
+
+        [Fact]
+        public void BinarySearchIterative_Should_Throw_If_Null()
+        {
+            //Arrange
+
+            //Act
+            Action act = () => _searching.BinarySearchIterative(null, 1);
+
+            //Assert
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void BinarySearchIterative_Should_Check_Empty()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 0).ToArray();
+
+            //Act
+            var result = _searching.BinarySearchIterative(array, 0);
+
+            //Assert
+            result.ShouldBeEquivalentTo(-1);
+        }
+
+        [Fact]
+        public void BinarySearchIterative_Should_Return_False_If_Not_Contains()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 1000000).ToArray();
+
+            //Act
+            var result = _searching.BinarySearchIterative(array, -5);
+
+            //Assert
+            result.ShouldBeEquivalentTo(-1);
+        }
+
+        [Fact]
+        public void BinarySearchIterative_Should_Check_Search_True()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 1000000).ToArray();
+            int item = 555555;
+            var index = Array.FindIndex(array, 0, x => x == item);
+
+            //Act
+            var result = _searching.BinarySearchIterative(array, item);
+
+            //Assert
+            result.ShouldBeEquivalentTo(index);
+        }
+
+        [Fact]
+        public void BinarySearchRecursive_Should_Throw_If_Null()
+        {
+            //Arrange
+
+            //Act
+            Action act = () => _searching.BinarySearchRecursive(null, 1);
+
+            //Assert
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void BinarySearchRecursive_Should_Check_Empty()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 0).ToArray();
+
+            //Act
+            var result = _searching.BinarySearchRecursive(array, 0);
+
+            //Assert
+            result.ShouldBeEquivalentTo(-1);
+        }
+
+        [Fact]
+        public void BinarySearchRecursive_Should_Return_False_If_Not_Contains()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 1000000).ToArray();
+
+            //Act
+            var result = _searching.BinarySearchRecursive(array, -5);
+
+            //Assert
+            result.ShouldBeEquivalentTo(-1);
+        }
+
+        [Fact]
+        public void BinarySearchRecursive_Should_Check_Search_True()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 1000000).ToArray();
+            int item = 555555;
+            var index = Array.FindIndex(array, 0, x => x == item);
+
+            //Act
+            var result = _searching.BinarySearchRecursive(array, item);
+
+            //Assert
+            result.ShouldBeEquivalentTo(index);
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/Algorithms/Searching.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Searching<T> where T : IComparable
+    {
+        public int BinarySearchIterative(IEnumerable<T> array, T item)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            var tempArray = array.ToArray();
+
+            int left = 0;
+            int right = tempArray.Length - 1;
+
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (tempArray[mid].CompareTo(item) == 0)
+                    return mid;
+
+                if (tempArray[mid].CompareTo(item) < 0)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return -1;
+        }
+
+        public int BinarySearchRecursive(IEnumerable<T> array, T item)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            var tempArray = array.ToArray();
+
+            return BinarySearchRecursiveHelper(tempArray, item, 0, tempArray.Length - 1);
+        }
+
+        private int BinarySearchRecursiveHelper(T[] array, T item, int left, int right)
+        {
+            if (left > right)
+                return -1;
+            var mid = (left + right) / 2;
+
+            int compared = array[mid].CompareTo(item);
+            if (compared == 0)
+                return mid;
+
+            if (compared < 0)
+            {
+                var rightResult = BinarySearchRecursiveHelper(array, item, mid + 1, right);
+                if (rightResult != -1)
+                    return rightResult;
+            }
+            else
+            {
+                var leftResult = BinarySearchRecursiveHelper(array, item, left, mid - 1);
+                if (leftResult != -1)
+                    return leftResult;
+            }
+
+            return -1;
+        }
+    }
+}

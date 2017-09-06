982e0e3d753939ab8616fa683e651ec72c87a12d
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-09-06 20:56:48 +0300

    Sorting and Searching - Counting Sort + Unit Tests

---
+++ CrackingTheCodingInterview/Algorithms.UT/CountingSortTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.UT
+{
+    public class CountingSortTests
+    {
+        [Fact]
+        public void Should_Check_Empty_Array()
+        {
+            //Arrange
+            var array = Enumerable.Range(0, 0).ToArray();
+
+            //Act
+            var result = array.CountingSorting().ToArray();
+
+            //Assert
+            result.Length.ShouldBeEquivalentTo(0);
+        }
+
+        [Fact]
+        public void Should_Check_Sort()
+        {
+            //Arrange
+            var random = new Random();
+            int count = 1000;
+            var array = new int[count];
+            for (int i = 0; i < count; i++)
+                array[i] = random.Next(0, 10);
+
+            //Act
+            var result = array.CountingSorting().ToArray();
+
+            //Assert
+            result.SequenceEqual(array.OrderBy(x => x))
+                .ShouldBeEquivalentTo(true);
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/Algorithms/CountingSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class CountingSort
+    {
+        public static IEnumerable<int> CountingSorting(this IEnumerable<int> array)
+        {
+            var tempArray = array.ToArray();
+
+            var buckets = new int[10];
+            foreach (var item in tempArray)
+                buckets[item]++;
+
+            for (int i = 1; i < buckets.Length; i++)
+                buckets[i] += buckets[i - 1];
+
+            var result = new int[tempArray.Length];
+            for (int i = tempArray.Length - 1; i >= 0; i--)
+            {
+                var resultIndex = buckets[tempArray[i]] - 1;
+                buckets[tempArray[i]]--;
+                result[resultIndex] = tempArray[i];
+            }
+
+            return result;
+        }
+    }
+}

c8c48b1bdf7ee7a97c875ca04d92c17ba763ed00
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-09-10 17:06:54 +0300

    Hard - Add Without Plus + Unit Tests

---
+++ CrackingTheCodingInterview/Tasks.UT/HardTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Tasks.UT
+{
+    public class HardTests
+    {
+        private readonly Hard _subject;
+        public HardTests()
+        {
+            _subject = new Hard();
+        }
+
+        [Fact]
+        public void AddWithPlus_Should_Add_Positive_Numbers()
+        {
+            //Arrange
+            int a = 5;
+            int b = 7;
+
+            //Act
+            var result = _subject.AddWithoutPlus(a, b);
+
+            //Assert
+            result.ShouldBeEquivalentTo(a + b);
+        }
+
+        [Fact]
+        public void AddWithPlus_Should_Add_Zero()
+        {
+            //Arrange
+            int a = 0;
+            int b = 7;
+
+            //Act
+            var result = _subject.AddWithoutPlus(a, b);
+
+            //Assert
+            result.ShouldBeEquivalentTo(a + b);
+        }
+
+        [Fact]
+        public void AddWithPlus_Should_Add_Negative_Number()
+        {
+            //Arrange
+            int a = -2;
+            int b = 7;
+
+            //Act
+            var result = _subject.AddWithoutPlus(a, b);
+
+            //Assert
+            result.ShouldBeEquivalentTo(a + b);
+        }
+
+        [Fact]
+        public void AddWithPlus_Should_Add_Two_Negative_Numbers()
+        {
+            //Arrange
+            int a = -2;
+            int b = -7;
+
+            //Act
+            var result = _subject.AddWithoutPlus(a, b);
+
+            //Assert
+            result.ShouldBeEquivalentTo(a + b);
+        }
+
+        [Fact]
+        public void AddWithPlus_Should_Check_Load_With_Random_Numbers()
+        {
+            //Arrange
+            int count = 10000;
+            var random = new Random();
+
+            //Assert
+            while (count-- >= 0)
+            {
+                var first = random.Next();
+                var second = random.Next();
+
+                var result = _subject.AddWithoutPlus(first, second);
+                result.ShouldBeEquivalentTo(first + second);
+            }
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/Tasks/Hard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Hard
+    {
+        public int AddWithoutPlus(int a, int b)
+        {
+            if (b == 0)
+                return a;
+
+            int c = a & b;
+            a = a ^ b;
+
+            return AddWithoutPlus(a, c << 1);
+        }
+    }
+}
